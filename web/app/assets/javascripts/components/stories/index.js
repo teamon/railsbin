92872e88fb8a278c1245633d02867430698faf94
Tymon Tobolski <i@teamon.eu>
2016-04-13 13:32:52 +0200

    [ui] Moved all component stories into single file

--- web/app/assets/javascripts/components/stories/index.js
+++ web/app/assets/javascripts/components/stories/index.js
@@ -1,2 +1,23 @@
-import "./Button"
-import "./BrowserFrame"
+import React from "react"
+import { storiesOf, action, linkTo } from "@kadira/storybook"
+
+import Button from "../Button"
+import BrowserFrame from "../BrowserFrame"
+
+storiesOf('Button', module)
+  .add('default', () =>
+    <div className="container">
+      <Button label="Click me" onClick={action('click the button')}/>
+      {" "}
+      <Button label="Click me" className="button button-outline" onClick={action('click the button')}/>
+      {" "}
+      <Button label="Click me" className="button button-clear" onClick={action('click the button')}/>
+    </div>
+  )
+
+storiesOf('BrowserFrame', module)
+  .add('default', () =>
+    <div className="container">
+      <BrowserFrame url="http://teamon.eu"/>
+    </div>
+  )
